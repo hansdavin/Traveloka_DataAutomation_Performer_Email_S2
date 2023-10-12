167e3025dd7dc3bce271ba478833ca881b99d59a
Hans Davin Christian <hans.davin@kalyana.co.id>
2023-10-12 10:44:32 +0700

    Change project dependency

---
+++ .local/install/CodedWorkflow.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+
+namespace Traveloka_DataAutomation_Email_S2
+{
+    public partial class CodedWorkflow : CodedWorkflowBase
+    {
+        public CodedWorkflow()
+        {
+            _ = new System.Type[]{};
+        }
+    }
+}
